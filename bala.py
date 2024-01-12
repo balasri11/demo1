b7a62714ab6ad35f66d61b5daa6c5c7ef99889d2
balasri11 <147686900+balasri11@users.noreply.github.com>
2024-01-12 10:37:33 +0530

    Create bala.py

---
+++ bala.py
@@ -0,0 +1,6 @@
+# demo
+streamlit app
+import streamlit as st
+st.header('puppy')
+st.write('puppy')
+st.image('https://tse4.mm.bing.net/th?id=OIP.TnnO-9C6THhBBCzOhTe7mQHaFj&pid=Api&P=0&h=180',caption='dog')
